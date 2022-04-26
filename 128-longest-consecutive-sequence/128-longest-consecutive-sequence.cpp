065050b99933da791bad056437ec5ce71ca09da7
Rakshit <37021771+rakshitgarg99@users.noreply.github.com>
2022-04-27 00:58:38 +0530

    Time: 180 ms (42.70%), Space: 31.8 MB (21.33%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -2,22 +2,38 @@ class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
         if(nums.size()==0) return 0;
-        // set<int> s(nums.begin(), nums.end());
-        sort(nums.begin(), nums.end());
+//         sort(nums.begin(), nums.end());
         
-        int max_len=0, temp=0;
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i]==nums[i+1]){}
-            else if(nums[i]==nums[i+1]-1){
-                // cout<<nums[i];
-                temp++;
-            }else{
-                max_len=max(max_len, temp);
-                temp=0;
+//         int max_len=0, temp=0;
+//         for(int i=0;i<nums.size()-1;i++){
+//             if(nums[i]==nums[i+1]){}
+//             else if(nums[i]==nums[i+1]-1){
+//                 // cout<<nums[i];
+//                 temp++;
+//             }else{
+//                 max_len=max(max_len, temp);
+//                 temp=0;
                 
+//             }
+//         }
+//         max_len=max(max_len, temp);
+//         return max_len+1;
+        
+        set<int> s(nums.begin(), nums.end());
+        int maxlen=0;
+        for(auto num:s){
+            if(!s.count(num-1)){
+                int currstreak=1;
+                int currnum=num;
+                
+                while(s.count(currnum+1)){
+                    currstreak++;
+                    currnum++;
+                }
+                
+                maxlen=max(maxlen, currstreak);
             }
         }
-        max_len=max(max_len, temp);
-        return max_len+1;
+        return maxlen;
     }
 };
