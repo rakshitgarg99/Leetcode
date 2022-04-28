ed613ba10f39e300fdd720e776b398f439aaa6c3
Rakshit <37021771+rakshitgarg99@users.noreply.github.com>
2022-04-28 18:43:41 +0530

    Time: 8 ms (88.02%), Space: 8.2 MB (72.92%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -19,21 +19,37 @@ public:
 //         }
 //         return ans;
         
-        unordered_set<char> st;
+//         unordered_set<char> st;
         
-        int i=0, j=0, n=s.size();
-        int ans=0;
+//         int i=0, j=0, n=s.size();
+//         int ans=0;
         
-        while(i<n && j<n){
-            if(st.find(s[j])==st.end()){
-                st.insert(s[j++]);
-                ans = max(ans, j-i);
+//         while(i<n && j<n){
+//             if(st.find(s[j])==st.end()){
+//                 st.insert(s[j++]);
+//                 ans = max(ans, j-i);
                 
+//             }
+//             else{
+//                 st.erase(s[i++]);
+//             }
+//         }
+//         return ans;
+        
+        vector<int> mp(256,-1);
+        int left=0, right=0, len=0, maxlen=0;
+        int n=s.size();
+        while(right<n){
+            if(mp[s[right]]!=-1){
+                left=max(mp[s[right]]+1, left);
             }
-            else{
-                st.erase(s[i++]);
-            }
+            
+            mp[s[right]]=right;
+            len=max(len, right-left+1);
+            right++;
         }
-        return ans;
+        return len;
+        
+        
     }
 };
