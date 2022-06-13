de137f18253cc042b05c718942040f82035ec6c9
Rakshit <37021771+rakshitgarg99@users.noreply.github.com>
2022-06-13 16:32:02 +0530

    Time: 13 ms (32.07%), Space: 7.3 MB (72.79%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
@@ -1,30 +1,55 @@
 class Solution {
 public:
-    string reverseWords(string s) {
+    string reverseWords(string str) {
         
-        stack<string> st;
-        string word="";
-        string ans="";
-        for(int i=0;i<s.size();i++){
-            while(i<s.size() and s[i]==' '){i++;}
+//         stack<string> st;
+//         string word="";
+//         string ans="";
+//         for(int i=0;i<s.size();i++){
+//             while(i<s.size() and s[i]==' '){i++;}
             
-            while(i<s.size() and s[i]!=' '){
-                word+=s[i];
-                i++;
+//             while(i<s.size() and s[i]!=' '){
+//                 word+=s[i];
+//                 i++;
+//             }
+//             st.push(word);
+//             word="";
+            
+//         }
+        
+//         while(!st.empty()){
+//             string top=st.top();
+//             st.pop();
+//             ans = ans+" "+top;
+//         }
+        
+//         int i=0;
+//         while(i<ans.size() and ans[i]==' '){i++;}
+//         return ans.substr(i,ans.size());
+        
+        
+        if(str==""||str==" ") return "";
+    string ans;
+    int i=str.size()-1;
+    
+    while(i>=0){
+        if(str[i]==' '){
+            i--;
+        }
+        else{
+            if(ans.size()>0){
+                ans.push_back(' ');
             }
-            st.push(word);
-            word="";
             
+            int j=i;
+            while(j>=0 and str[j]!=' '){
+                j--;
+            }
+            ans.append(str.substr(j+1,i-j));
+            i=j;
         }
+    }
+    return ans;
         
-        while(!st.empty()){
-            string top=st.top();
-            st.pop();
-            ans = ans+" "+top;
-        }
-        
-        int i=0;
-        while(i<ans.size() and ans[i]==' '){i++;}
-        return ans.substr(i,ans.size());
     }
 };
