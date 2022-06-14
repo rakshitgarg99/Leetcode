bd0f7d5d1c4bc4b4ee8da49b196a0f42246e3fd2
Rakshit <37021771+rakshitgarg99@users.noreply.github.com>
2022-06-14 23:25:30 +0530

    Time: 27 ms (12.37%), Space: 46.9 MB (8.12%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
@@ -1,55 +1,55 @@
 class Solution {
 public:
-    string reverseWords(string str) {
+    string reverseWords(string s) {
         
-//         stack<string> st;
-//         string word="";
-//         string ans="";
-//         for(int i=0;i<s.size();i++){
-//             while(i<s.size() and s[i]==' '){i++;}
+        stack<string> st;
+        string word="";
+        string ans="";
+        for(int i=0;i<s.size();i++){
+            while(i<s.size() and s[i]==' '){i++;}
             
-//             while(i<s.size() and s[i]!=' '){
-//                 word+=s[i];
-//                 i++;
-//             }
-//             st.push(word);
-//             word="";
+            while(i<s.size() and s[i]!=' '){
+                word+=s[i];
+                i++;
+            }
+            st.push(word);
+            word="";
             
-//         }
+        }
         
-//         while(!st.empty()){
-//             string top=st.top();
-//             st.pop();
-//             ans = ans+" "+top;
-//         }
+        while(!st.empty()){
+            string top=st.top();
+            st.pop();
+            ans = ans+" "+top;
+        }
         
-//         int i=0;
-//         while(i<ans.size() and ans[i]==' '){i++;}
-//         return ans.substr(i,ans.size());
+        int i=0;
+        while(i<ans.size() and ans[i]==' '){i++;}
+        return ans.substr(i,ans.size());
         
         
-        if(str==""||str==" ") return "";
-    string ans;
-    int i=str.size()-1;
+//         if(str==""||str==" ") return "";
+//     string ans;
+//     int i=str.size()-1;
     
-    while(i>=0){
-        if(str[i]==' '){
-            i--;
-        }
-        else{
-            if(ans.size()>0){
-                ans.push_back(' ');
-            }
+//     while(i>=0){
+//         if(str[i]==' '){
+//             i--;
+//         }
+//         else{
+//             if(ans.size()>0){
+//                 ans.push_back(' ');
+//             }
             
-            int j=i;
-            while(j>=0 and str[j]!=' '){
-                j--;
-            }
-            ans.append(str.substr(j+1,i-j));
-            i=j;
-        }
-    }
-    return ans;
+//             int j=i;
+//             while(j>=0 and str[j]!=' '){
+//                 j--;
+//             }
+//             ans.append(str.substr(j+1,i-j));
+//             i=j;
+//         }
+//     }
+//     return ans;
         
     }
 };
