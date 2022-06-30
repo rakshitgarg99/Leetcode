f644d03c532bad3fe57f99caf5b1fbe8bbb9a6a4
Rakshit <37021771+rakshitgarg99@users.noreply.github.com>
2022-06-30 22:51:31 +0530

    Added solution - LeetHub

--- Topological sort - GFG/topological-sort.cpp
+++ Topological sort - GFG/topological-sort.cpp
@@ -7,32 +7,62 @@ class Solution
 {
 	public:
 	//Function to return list containing vertices in Topological order. 
+	
+	
+	void solve(vector<int> adj[],int i,vector<int> &vis,stack<int>&st){
+    vis[i]=1;
+    
+    for(auto x:adj[i]){
+        if(!vis[x]){
+            solve(adj,x,vis,st);
+        }
+    }
+    
+    st.push(i);
+    
+}
 	vector<int> topoSort(int v, vector<int> adj[]) 
 	{
 	    // code here
 	    
-	    vector<int> indegree(v,0);
-	    for(int i=0;i<v;i++){
-	        for(auto x:adj[i]) indegree[x]++;
-	    }
-	    queue<int> q;
-	    for(int i=0;i<v;i++){
-	        if(indegree[i]==0) q.push(i);
-	    }
+	   // vector<int> indegree(v,0);
+	   // for(int i=0;i<v;i++){
+	   //     for(auto x:adj[i]) indegree[x]++;
+	   // }
+	   // queue<int> q;
+	   // for(int i=0;i<v;i++){
+	   //     if(indegree[i]==0) q.push(i);
+	   // }
 	    
-	    vector<int> topo;
-	    while(!q.empty()){
-	        int node=q.front();
-	        q.pop();
-	        topo.push_back(node);
+	   // vector<int> topo;
+	   // while(!q.empty()){
+	   //     int node=q.front();
+	   //     q.pop();
+	   //     topo.push_back(node);
 	        
-	        for(auto x: adj[node]){
-	            indegree[x]--;
-	            if(indegree[x]==0) q.push(x);
-	        }
-	    }
+	   //     for(auto x: adj[node]){
+	   //         indegree[x]--;
+	   //         if(indegree[x]==0) q.push(x);
+	   //     }
+	   // }
 	    
-	    return topo;
+	   // return topo;
+	   
+	   
+	vector<int> vis(v+1,0);
+    stack<int> st;
+    for(int i=0;i<v;i++){
+        if(!vis[i]){
+            solve(adj,i,vis,st);
+        }
+    }
+    
+    vector<int> topo;
+    while(!st.empty()){
+        topo.push_back(st.top());
+        st.pop();
+    }
+    return topo;
 	}
 };
 
