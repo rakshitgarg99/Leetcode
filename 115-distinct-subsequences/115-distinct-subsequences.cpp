806310490cd555057200b116f8bc744633e0a001
Rakshit <37021771+rakshitgarg99@users.noreply.github.com>
2022-07-08 18:43:45 +0530

    Time: 51 ms (61.43%), Space: 30 MB (26.66%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -15,33 +15,27 @@ public:
 //         }
 //     }
     int numDistinct(string s, string t) {
-//         int n=s.size();
-//         int m=t.size();
-//         // vector<vector<int>> dp(n+1, vector<int>(m+1,-1));
+        int n=s.size();
+        int m=t.size();
+        // vector<vector<int>> dp(n+1, vector<int>(m+1,-1));
         
-//         // return solve(s,t,n,m,dp);
-//         vector<vector<double>> dp(n+1, vector<double>(m+1,0));
-//         for(int i=0;i<=n;i++){
-//             dp[i][0]=1;
-//         }
+        // return solve(s,t,n,m,dp);
+        vector<vector<double>> dp(n+1, vector<double>(m+1,0));
+        for(int i=0;i<=n;i++){
+            dp[i][0]=1;
+        }
         
-//         for(int i=1;i<=n;i++){
-//             for(int j=1;i<=m;j++){
-//                 if(s[i-1]==t[j-1]){
-//                     dp[i][j]= dp[i-1][j-1] + dp[i-1][j];
-//                 }
-//                 else{
-//                     dp[i][j] = dp[i-1][j];
-//                 }
-//             }
-//         }
-//         return (int)dp[n][m];
-        int m = t.length(), n = s.length();
-        vector<vector<double>> dp(m + 1, vector<double> (n + 1, 0));
-        for (int j = 0; j <= n; j++) dp[0][j] = 1;
-        for (int j = 1; j <= n; j++)
-            for (int i = 1; i <= m; i++)
-                dp[i][j] = dp[i][j - 1] + (t[i - 1] == s[j - 1] ? dp[i - 1][j - 1] : 0);
-        return (int)dp[m][n];
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1]==t[j-1]){
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return (int)dp[n][m];
+
     }
 };
