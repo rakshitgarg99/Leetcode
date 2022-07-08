0f12d6b2ae6853ca02074a4c8bd609f7cb2d4a8d
Rakshit <37021771+rakshitgarg99@users.noreply.github.com>
2022-07-09 01:01:27 +0530

    Time: 74 ms (20.06%), Space: 82 MB (5.35%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -3,10 +3,10 @@ public:
     
     int solve(string word1, string word2, int m, int n, vector<vector<int>> &dp){
         // if(m==0||n==0) return 0;
-        if(m<0) return n+1;
-        if(n<0) return m+1;
+        if(m==0) return n;
+        if(n==0) return m;
         if(dp[m][n]!=-1) return dp[m][n];
-        if(word1[m]==word2[n]){
+        if(word1[m-1]==word2[n-1]){
             return dp[m][n]=solve(word1, word2, m-1,n-1,dp);
         }
         else{
@@ -21,7 +21,7 @@ public:
         int m=word1.size();
         int n=word2.size();
         vector<vector<int>> dp(m+1, vector<int>(n+1,-1));
-        int ans=solve(word1, word2, m-1, n-1, dp);
+        int ans=solve(word1, word2, m, n, dp);
         return ans;
     }
 };
