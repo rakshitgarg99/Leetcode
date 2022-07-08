2a5fa93368053298897f271cb745e3f40aa1354e
Rakshit <37021771+rakshitgarg99@users.noreply.github.com>
2022-07-09 01:00:21 +0530

    Time: 71 ms (20.46%), Space: 82 MB (5.35%) - LeetHub

---
+++ 72-edit-distance/72-edit-distance.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    
+    int solve(string word1, string word2, int m, int n, vector<vector<int>> &dp){
+        // if(m==0||n==0) return 0;
+        if(m<0) return n+1;
+        if(n<0) return m+1;
+        if(dp[m][n]!=-1) return dp[m][n];
+        if(word1[m]==word2[n]){
+            return dp[m][n]=solve(word1, word2, m-1,n-1,dp);
+        }
+        else{
+            int i=1+solve(word1,word2,m-1,n,dp);
+            int d=1+solve(word1,word2,m,n-1,dp);
+            int r=1+solve(word1,word2,m-1,n-1,dp);
+            return dp[m][n]=min(i,min(d,r));
+        }
+    }
+    
+    int minDistance(string word1, string word2) {
+        int m=word1.size();
+        int n=word2.size();
+        vector<vector<int>> dp(m+1, vector<int>(n+1,-1));
+        int ans=solve(word1, word2, m-1, n-1, dp);
+        return ans;
+    }
+};
