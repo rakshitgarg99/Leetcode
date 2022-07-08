b1fe716b3fa27bdd34de1f888bc15365021bea81
Rakshit <37021771+rakshitgarg99@users.noreply.github.com>
2022-07-08 17:00:38 +0530

    Time: 18 ms (74.28%), Space: 10.6 MB (86.08%) - LeetHub

---
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    string shortestCommonSupersequence(string str1, string str2) {
+        int m = str1.size();
+        int n = str2.size();
+        
+        int dp[m+1][n+1];
+        
+        for(int i=0;i<=m;i++) dp[i][0]=0;
+        for(int i=1;i<=n;i++) dp[0][i]=0;
+        
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(str1[i-1]==str2[j-1]){
+                    dp[i][j] = 1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        
+        string s="";
+        int i=m, j=n;
+        while(i>0 && j>0){
+            if(str1[i-1]==str2[j-1]){
+                s.push_back(str1[i-1]);
+                i--;
+                j--;
+            }
+            else{
+                if(dp[i-1][j]>dp[i][j-1]){
+                    s.push_back(str1[i-1]);
+                    i--;
+                }
+                else{
+                    s.push_back(str2[j-1]);
+                    j--;
+                }
+            }
+        }
+        
+        while(i>0){
+            s.push_back(str1[i-1]);
+            i--;
+        }
+        while(j>0){
+            s.push_back(str2[j-1]);
+            j--;
+        }
+        reverse(s.begin(), s.end());
+        return s;
+    }
+};
