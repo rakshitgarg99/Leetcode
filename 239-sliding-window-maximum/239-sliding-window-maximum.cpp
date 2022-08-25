1b821ebac7499862a18d344f3bcd6492ecd2160e
Rakshit <37021771+rakshitgarg99@users.noreply.github.com>
2022-08-26 02:17:18 +0530

    Time: 407 ms (71.50%), Space: 136.9 MB (38.95%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -1,44 +1,43 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-//         deque<int> q;
-//         vector<int> v;
-        
-//         int i=0, j=0;
-//         while(j<nums.size()){
-//             while(!q.empty() and q.back()<nums[j]){
-//                 q.pop_back();
-//             }
-//             q.push_back(nums[j]);
             
-//             if(j-i+1<k) j++;
-            
-//             else if(j-i+1==k){
-//                 v.push_back(q.front());
-//                 if(q.front()==nums[i]){
-//                     q.pop_front();
-//                 }
-//                 i++;
-//                 j++;
-//             }
-//         }
-//         return v;
-        
-        
-        priority_queue<pair<int, int>> heap;
+        deque<pair<int, int>> dq;
         vector<int> ans;
         
         for(int i=0;i<nums.size();i++){
-            while(!heap.empty() and heap.top().second <= (i-k)){
-                heap.pop();
+            
+            if(!dq.empty() and dq.front().second <= (i-k)){
+                dq.pop_front();
             }
             
-            heap.push({nums[i], i});
-            if(i>=k-1){
-                ans.push_back(heap.top().first);
+            while(!dq.empty() and dq.back().first <= nums[i]){
+                dq.pop_back();
+            }
+            
+            dq.push_back({nums[i], i});
+            if(i>= k-1){
+                ans.push_back(dq.front().first);
             }
         }
+        
         return ans;
+        
+        
+//         priority_queue<pair<int, int>> heap;
+//         vector<int> ans;
+        
+//         for(int i=0;i<nums.size();i++){
+//             while(!heap.empty() and heap.top().second <= (i-k)){
+//                 heap.pop();
+//             }
+            
+//             heap.push({nums[i], i});
+//             if(i>=k-1){
+//                 ans.push_back(heap.top().first);
+//             }
+//         }
+//         return ans;
         
     }
 };
